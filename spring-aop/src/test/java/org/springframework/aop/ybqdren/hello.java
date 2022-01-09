fc1dcacee93eb39104b68b5c48af86350a4b2998
Wen Zhao (Joan) <withzhaowen@126.com>
2022-01-09 11:31:44 +0800

    update 参考 spring 编译文章：https://blog.csdn.net/guyexiangyun/article/details/106527732

---
+++ spring-aop/src/test/java/org/springframework/aop/ybqdren/hello.java
@@ -0,0 +1,17 @@
+package org.springframework.aop.ybqdren;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Wen(Joan) Zhao
+ * @time 2022/1/9 11:13
+ * @package org.springframework.aop.ybqdren
+ * @description
+ **/
+public class hello {
+
+	@Test
+	public void testHello(){
+		System.out.println("hello ~");
+	}
+}
